482b0ef0f16213796d22dfa3449c45ea35a60481
weijie <jeremy.sun@eventiq.com>
2018-09-10 14:47:53 -0600

    Init the component

--- src/main/resources/react-app/src/Router.tsx
+++ src/main/resources/react-app/src/Router.tsx
@@ -6,6 +6,9 @@ import createHistory from 'history/createBrowserHistory';
 import Index from './pages/index';
 import Home from './Components/Home';
 import Profile from './Components/Profile';
+import Classroom from './Components/Classroom';
+import AttendanceList from './Components/AttendanceList';
+import SetupClassRoom from './Components/SetupClassRoom';
 // import Schedule from "./Schedule.js"
 
 const history = createHistory();
@@ -19,9 +22,9 @@ class ReactRouter extends React.Component {
                     <Route path="/index" component={Index} />
                     <Route path="/home" component={Home} />
                     <Route path="/profile" component={Profile} />
-                    {/*<Route path="/classroom/:classroomId" component={Classroom} />*/}
-                    {/*<Route path="/classroom/:classroomId/attendance-list/:listId" component={AttendanceList} />*/}
-                    {/*<Route path="/setup/classroom/:id" component={SetupClassRoom} />*/}
+                    <Route path="/classroom/:classroomId" component={Classroom} />
+                    <Route path="/classroom/:classroomId/attendance-list/:listId" component={AttendanceList} />
+                    <Route path="/setup/classroom/:id" component={SetupClassRoom} />
 
                     {/* 404 not fond page, make sure it stay at bottom */}
                     <Route path="*" component={NotFound} />
